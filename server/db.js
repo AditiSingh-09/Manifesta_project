d16f3a9556cd3426c7fabd3698cdd8fd02994dbf
AditiSingh-09 <asaditisingh203@gmail.com>
2025-05-31 21:45:19 +0530

    initial

--- server/db.js
+++ server/db.js
@@ -5,12 +5,12 @@ const Pool  = require("pg").Pool;
 //require("dotenv").config(); // Load environment variables
 
 const pool = new Pool({
-  user: "postgres",        // Database user
-  host: "localhost",        // Database host (usually localhost for local dev)
-  database: "manifesta",    // Database name
-  password: "sky", // Database password
+  user: "postgres",        
+  host: "localhost",        
+  database: "manifesta",    
+  password: "sky", 
   port: 5432,
-          // Database port (default PostgreSQL port is 5432)
+       
 });
 
 
